d7c7e6aa99df5d542b39b858fe480dbf5fb8e509
Blaze Lim <blazebentleycolim@gmail.com>
2021-04-28 21:53:43 -0700

    added app.listen to server.js

--- server.js
+++ server.js
@@ -19,4 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+// html routes
 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
