a238c5d4463a1d2a45961b465b0f305ffbafc22e
Blaze Lim <blazebentleycolim@gmail.com>
2021-04-28 22:08:02 -0700

    added html paths for notes and index.html

--- server.js
+++ server.js
@@ -19,7 +19,27 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+// api routes
+app.get('/api/notes', (req, res) => {
+    let results = savedNotes;
+
+    res.json(results);
+})
+
+
 // html routes
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+// html catch all
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
